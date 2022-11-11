8c6c9715305f1262183fd63cff09d4568b9532f7
Siva Prakash <prakashdeveloper@outlook.in>
2022-11-11 12:24:53 +0530

    Added imgs folder & Updated app.py

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
+import os
+import pandas as pd
 import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
+from pycaret.regression import setup, compare_models, pull, save_model
 
 # setting app's title, icon & layout
-st.set_page_config(page_title="Page Title", page_icon="🎯")
+st.set_page_config(page_title="AutoML", page_icon="⚡")
 
+# sample dataset
+if os.path.exists("./data/dataset.csv"):
+    df = pd.read_csv("./data/dataset.csv", index_col=None)
 
-def main():
-    # display app header
-    st.header("Header")
+with st.sidebar:
+    st.image("./imgs/inno2.png")
+    st.title("AutoML")
+    choice = st.selectbox(
+        "Navigation", ["Upload", "Profiling", "Modelling", "Download"]
+    )
+    st.info("This project application helps you build and explore your data.")
 
+match choice:
+    case "Upload":
+        st.title("Upload Your Dataset")
+        if file := st.file_uploader("Upload Your Dataset"):
+            df = pd.read_csv(file, index_col=None)
+            df.to_csv("data/dataset.csv", index=None)
+            st.dataframe(df)
 
-if __name__ == "__main__":
-    main()  # calls the main() first
+    case "Profiling":
+        st.title("Exploratory Data Analysis")
+        profile_df = df.profile_report()
+        st_profile_report(profile_df)
+
+    case "Modelling":
+        chosen_target = st.selectbox("Choose the Target Column", df.columns)
+        if st.button("Run Modelling"):
+            setup(df, target=chosen_target, silent=True)
+            setup_df = pull()
+            st.dataframe(setup_df)
+            best_model = compare_models()
+            compare_df = pull()
+            st.dataframe(compare_df)
+            save_model(best_model, "model/best_model")
+
+    case "Download":
+        with open("model/best_model.pkl", "rb") as f:
+            st.download_button("Download Model", f, file_name="model/best_model.pkl")
--- readme.md
+++ readme.md
@@ -1,5 +1,34 @@
 # AutoML Streamlit
-![Python](https://img.shields.io/badge/Python-0078D7.svg?logo=python&logoColor=white)
-![streamlit](https://img.shields.io/badge/Streamlit-FF4B4B?logo=Streamlit&logoColor=white)
-![terminal](https://img.shields.io/badge/Windows%20Terminal-4D4D4D?logo=windows%20terminal&logoColor=white)
-![vscode](https://img.shields.io/badge/Visual_Studio_Code-0078D4?logo=visual%20studio%20code&logoColor=white)
+![made-with-python](https://img.shields.io/badge/Made%20with-Python-0078D4.svg)
+![pandas](https://img.shields.io/badge/Pandas-2C2D72?logo=pandas&logoColor=white)
+![streamlit](https://img.shields.io/badge/Streamlit-FF4B4B?logo=streamlit&logoColor=white)
+![terminal](https://img.shields.io/badge/Windows%20Terminal-4D4D4D?logo=Windows%20terminal&logoColor=white)
+![vscode](https://img.shields.io/badge/Visual_Studio_Code-0078D4?&logo=visual%20studio%20code&logoColor=white)
+
+AutoML app is used to analyse, visualize and built best machine learning regression model for your dataset. You can download the model in `pkl` and pandas profiling report in `html` format.
+
+## Installation
+Open command prompt and create new environment
+```
+conda create -n your_env_name python=<version>
+```
+Then Activate the newly created environment
+```
+conda activate your_env_name
+```
+Clone the repository using `git`
+```
+git clone https://github.com/Prakashdeveloper03/AutoML-Streamlit.git
+```
+Change to the cloned directory
+```
+cd <directory_name>
+```
+Then install all requirement packages for the app
+```
+pip install -r requirements.txt
+```
+Then, Run the `app.py` script
+```
+streamlit run app.py
+```
